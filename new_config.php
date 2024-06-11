922b28a6a175f1a0e3a8e1103d80dc6c6bd849e6
owonikoko Ayomide <donaykoko@yahoo.com>
2024-06-11 13:15:54 +0100

    Update new_config.php

--- new_config.php
+++ new_config.php
@@ -3,7 +3,7 @@
     define('db_host', 'localhost');
     define('db_user', 'root');
     define('db_pass', '');
-    define('db_name', 'gallery_db');
+    define('db_name', 'gallery_db'); 
     
     $connection = mysqli_connect(db_host,db_user,db_pass,db_name) ;
     if($connection){
